f5c4c8fd11c706f7d6e223ec4523a512525962f3
Joseph <joseph.dillon.522@gmail.com>
2017-11-21 12:53:46 -0500

    added some mock helpers and tag page tests

--- src/app/pages/tags/tags.component.spec.ts
+++ src/app/pages/tags/tags.component.spec.ts
@@ -1,25 +1,61 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import * as fromRoot from '../../core/store/application-state';
+import { StoreModule, Store, combineReducers } from '@ngrx/store';
+import * as fromTag from '../../core/store/reducers/tag.reducer';
+import { MatMenuModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { TimeseriesService } from '../../services/timeseries/timeseries.service';
+import { HttpClientModule } from '@angular/common/http';
+import * as TagActions from '../../core/store/actions/tag.actions';
+import { By } from '@angular/platform-browser';
 import { TagsComponent } from './tags.component';
+import { DataTableModule } from 'primeng/primeng';
+import { SelectTag } from '../../core/store/actions/tag.actions';
+import { TagMock } from '../../testingHelpers/tag.mock';
 
 describe('TagsComponent', () => {
+  let store: Store<fromRoot.State>;
   let component: TagsComponent;
   let fixture: ComponentFixture<TagsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TagsComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        TagsComponent
+      ],
+      imports: [
+        DataTableModule,
+        HttpClientModule,
+        RouterModule,
+        MatMenuModule,
+        StoreModule.forRoot({
+          ...fromRoot.reducers,
+          'tag': combineReducers(fromTag.reducer)
+        })
+
+      ],
+      providers: [
+      ]
+    }).compileComponents();
+
+    store = TestBed.get(Store);
+
+    spyOn(store, 'dispatch').and.callThrough();
+
+    fixture = TestBed.createComponent(TagsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   }));
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(TagsComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should dispatch an action to load data when tag row is selected', () => {
+    const action = new TagActions.SelectTag(TagMock[0], true);
+    component.selected = TagMock[0];
+    component.onRowSelect();
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
 });
---
+++ src/app/testingHelpers/tag.mock.ts
@@ -0,0 +1,15 @@
+export const TagMock = [
+    {
+        'tagId': 'Tag1',
+        'label': 'Power at Meter 1',
+        'dataType': 'double',
+        'unit': 'kW',
+        'isTransient': false,
+        'features': [
+            'power',
+            'meter',
+            'load',
+            'consumption'
+        ]
+    }
+]
