fd3a551a0e4640e56dedc735561c1783ef977a90
Joseph <joseph.dillon.522@gmail.com>
2017-11-21 21:49:16 -0500

    added some more details tests

--- src/app/pages/details/details.component.spec.ts
+++ src/app/pages/details/details.component.spec.ts
@@ -1,25 +1,82 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import * as fromRoot from '../../core/store/application-state';
+import * as fromDetails from '../../core/store/reducers/details.reducer';
+import * as DetailsActions from '../../core/store/actions/details.actions';
 import { DetailsComponent } from './details.component';
+import { Store, StoreModule, combineReducers } from '@ngrx/store';
+import { DataTableModule, ChartModule } from 'primeng/primeng';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, MatInputModule } from '@angular/material';
+import { DatePickerDate, FormattedDatePickerDate, FormattedDate2 } from '../../testingHelpers/datepicker.mock';
+import { TagMock } from '../../testingHelpers/tag.mock';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DetailsComponent', () => {
+  let store: Store<fromRoot.State>;
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DetailsComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        DetailsComponent
+      ],
+      imports: [
+        DataTableModule,
+        HttpClientModule,
+        RouterModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatNativeDateModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        ChartModule,
+        StoreModule.forRoot({
+          ...fromRoot.reducers,
+          'tag': combineReducers(fromDetails.reducer)
+        })
+
+      ],
+      providers: [
+      ]
+    }).compileComponents();
+
+    store = TestBed.get(Store);
+
+    spyOn(store, 'dispatch').and.callThrough();
+
+    fixture = TestBed.createComponent(DetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   }));
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(DetailsComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  describe('Datepicker changes', () => {
+    it('should format the start date correctly', () => {
+      component.tagMeta = TagMock[0];
+      component.end = FormattedDate2;
+      component.onDateChange(DatePickerDate, 'start');
+      expect(component.start).toBe(FormattedDatePickerDate);
+    });
+
+    it('should format the end date correctly', () => {
+      component.tagMeta = TagMock[0];
+      component.start = FormattedDate2;
+      component.onDateChange(DatePickerDate, 'end');
+      expect(component.end).toBe(FormattedDatePickerDate);
+    });
+
+    it('should dispatch an action to get tag data when the date changes', () => {
+      component.tagMeta = TagMock[0];
+      component.end = FormattedDate2;
+      const action = new DetailsActions.GetDetails(TagMock[0].tagId, FormattedDatePickerDate, FormattedDate2);
+      component.onDateChange(DatePickerDate, 'start');
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    });
+  });
+
 });
---
+++ src/app/testingHelpers/datepicker.mock.ts
@@ -0,0 +1,6 @@
+export const DatePickerDate = {
+    value: 'Wed Nov 15 2017 00:00:00 GMT-0500 (EST)'
+};
+
+export const FormattedDatePickerDate = '2017-11-15';
+export const FormattedDate2 = '2017-11-18';
